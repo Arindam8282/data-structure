e97b23499a197d7a157a5db1e2688c9e565373a0
Arindam8282 <darindam507@gmail.com>
2021-02-18 20:27:41 +0530

    ushing after long time

--- recursions/palindrome.cpp
+++ recursions/palindrome.cpp
@@ -9,9 +9,15 @@ string palindrome(string str) {
   if(rev(str) == str) return "PALINDROME";
   else return "NOT PALINDROME";
 }
+
+
 int main() {
   string txt;
-  cout<<"Enter a word : ";
-  cin>>txt;
-  cout<<palindrome(txt);
+  int p=0,np=0;
+  int s = 4200;
+  int e = 19000;
+  for(int i=s;i<=e;i++)   
+  if(palindrome(to_string(i))=="PALINDROME") p++;
+  else np++;
+  cout<<"number of Palindrome : "<<p<<"\nnumber of not : "<<np;
 }
