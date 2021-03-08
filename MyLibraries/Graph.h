aa1c1cc05b0baacbcfadb11da756fd9d062c4c61
Arindam8282 <darindam507@gmail.com>
2021-03-08 20:53:59 +0530

    i dont know

---
+++ MyLibraries/Graph.h
@@ -0,0 +1,42 @@
+class Node{
+  public:
+  int data;
+  Node* next;
+  Node(int dt) {
+    data = dt;
+    next = NULL;
+  }
+};
+class Graph{
+  public:
+  Node *graph[5];
+  int vertices;
+  Graph(int v) {
+    vertices = v;
+    Node *graph[vertices];
+
+  }
+  void add_edge(int src,int data) {
+    Node *node = new Node(data);
+    node->next =  graph[src];
+    graph[src] = node;
+    // cout<<"\n\n"<<src<<"->"<<node->data;
+
+    node = new Node(src);
+    node->next = graph[data];
+    graph[data] = node;
+    // cout<<"\n\n"<<data<<"<-"<<node->data;
+  }
+  void print(Node *trav) {
+    if(trav!=NULL) print(trav->next);
+    else return;
+    cout<<trav->data<<"->";
+  }
+  void print_graph() {
+    for(int i = 0;i<vertices;i++) {
+      cout<<"\n\n"<<"Adjacency list of Vertex "<<i<<endl<<"head -> ";
+      print(graph[i]);
+    }
+      
+  }
+};
